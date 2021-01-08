29ac9a6313d7426d028c2fb7daa149d2ef312523
Franklyn De La Cruz <delacruzfranklyn93@users.noreply.github.com>
2021-01-08 18:31:47 -0500

    Adding updates to Bank.py and adding Bank.txt which has the Analysis results

--- PyBank/Bank.py
+++ PyBank/Bank.py
@@ -17,6 +17,7 @@ month_decrease = ""
 # Read in the data into a list
 
 csv_path = os.path.join( "Resources", "budget_data.csv")
+txt_outpath = os.path.join("Analysis", "bank.txt")
 
 with open(csv_path) as csv_file:
     budget_data = csv.reader(csv_file, delimiter = ",")
@@ -48,21 +49,20 @@ with open(csv_path) as csv_file:
             past = int(row[1])
         else:
             past = int(row[1])
+
+# Calculate the avg_change using sum and len
 avg_change = round(sum(avg_change)/len(avg_change), 2)
-print(avg_change)
-print(month_increase)
-print(greatest_increase)
-print(month_decrease)
-print(greatest_decrease)
 
-   
-          
+# Open a new file where we will write our Analysis to and start writing
+with open(txt_outpath, "w") as outfile:
+    outfile.write("Financial analysis\n")
+    outfile.write("-----------------------------\n")
+    outfile.write(f"Total Months: {months}\n")
+    outfile.write(f"Average Change: ${avg_change}\n")
+    outfile.write(f"Greatest Increase in Profits: {month_increase} (${greatest_increase})\n")
+    outfile.write(f"Greatest Decrease in Profits: {month_decrease} (${greatest_decrease})\n")
 
-
-
-
-
-
-
-
+# Open the new file you just created and also print the results on the terminal 
+with open(txt_outpath) as print_file:
+    print(print_file.read())
 
